78dcd10a1c28eb0b91782eb561f1c42e728a7821
e2xist <e2xist@gmail.com>
2020-09-04 06:11:54 +0900

    migrate 작업

--- app/src/main/java/kr/asv/androidutils/AdmobAdapter.kt
+++ app/src/main/java/kr/asv/androidutils/AdmobAdapter.kt
@@ -7,13 +7,13 @@ import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.InterstitialAd
 
 /**
- * Admob 관련 클래스
+ * AdMob 관련 클래스
  * Created by EXIZT on 2017-11-14.
  */
 class AdmobAdapter {
     companion object {
         /**
-         * 구글 광고 추가할 때에.
+         * 구글 배너 광고 추가할 때에 사용하는 메서드
          */
         @Suppress("unused")
         fun loadBannerAdMob(mAdView: AdView) {
@@ -40,9 +40,9 @@ class AdmobAdapter {
         @Suppress("unused", "SpellCheckingInspection")
         private fun newAdRequest(): AdRequest {
             val builder = AdRequest.Builder()
-            // builder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-            //builder.addTestDevice("621CBEEDE09F6A5B37180A718E74C41C");// G pro code
-            //builder.addTestDevice("2D81264572D2AB096C895509EDBD419F");// 확인 필요
+            //builder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR) // 이제는 에뮬레이터는 기본으로 지정되어 있으니, 넣으면 오히려 에러나는 구문.
+            //builder.addTestDevice("621CBEEDE09F6A5B37180A718E74C41C");// G pro 테스트 기기
+            //builder.addTestDevice("2D81264572D2AB096C895509EDBD419F");// Samsung G3 테스트 기기
             return builder.build()
         }
 
